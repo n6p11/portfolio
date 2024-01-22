1b80b2e675b7cb64cd2a9a638938da57bc4ab322
Niklāvs Petraškevics <niklavs.p@gmail.com>
2024-01-22 15:39:59 +0200

    feat: adding Navbar and Hero components

---
+++ src/data.jsx
@@ -0,0 +1,56 @@
+import { FaHtml5, FaJs, FaReact } from 'react-icons/fa'
+
+export const links = [
+  { id: 0, href: '#home', text: 'home' },
+  { id: 1, href: '#skills', text: 'skills' },
+  { id: 2, href: '#about', text: 'about' },
+  { id: 3, href: '#projects', text: 'projects' },
+]
+
+export const skills = [
+  {
+    id: 0,
+    title: 'HTML&CSS',
+    icon: <FaHtml5 className="h-16 w-16 text-emerald-500" />,
+    text: 'Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.',
+  },
+  {
+    id: 1,
+    title: 'Javascript',
+    icon: <FaJs className="h-16 w-16 text-emerald-500" />,
+    text: 'Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality',
+  },
+  {
+    id: 2,
+    title: 'React',
+    icon: <FaReact className="h-16 w-16 text-emerald-500" />,
+    text: 'Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.',
+  },
+]
+
+export const projects = [
+  {
+    id: 0,
+    img: 'https://images.pexels.com/photos/326503/pexels-photo-326503.jpeg?auto=compress&cs=tinysrgb&w=800',
+    url: 'https://react-projects.netlify.app/',
+    github: 'https://github.com/john-smilga',
+    title: 'first project',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.',
+  },
+  {
+    id: 1,
+    img: 'https://images.pexels.com/photos/2148222/pexels-photo-2148222.jpeg?auto=compress&cs=tinysrgb&w=800',
+    url: 'https://react-projects.netlify.app/',
+    github: 'https://github.com/john-smilga',
+    title: 'second project',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.',
+  },
+  {
+    id: 2,
+    img: 'https://images.pexels.com/photos/12883026/pexels-photo-12883026.jpeg?auto=compress&cs=tinysrgb&w=800',
+    url: 'https://react-projects.netlify.app/',
+    github: 'https://github.com/john-smilga',
+    title: 'third project',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.',
+  },
+]
